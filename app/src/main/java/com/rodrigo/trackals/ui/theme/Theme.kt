45d34693cb8dfe3283fcc78e030d6998123d8a8e
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2022-09-15 11:08:27 -0300

    setting up WelcomeScreen and Utils

--- app/src/main/java/com/rodrigo/trackals/ui/theme/Theme.kt
+++ app/src/main/java/com/rodrigo/trackals/ui/theme/Theme.kt
@@ -5,7 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import com.rodrigo.core_ui.Dimensions
+import com.rodrigo.core_ui.LocalSpacing
 
 private val DarkColorPalette = darkColors(
     primary = BrightGreen,
@@ -38,11 +41,12 @@ fun TrackalsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
     } else {
         LightColorPalette
     }
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Dimensions()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
---
+++ onboarding/onboarding_presentation/src/main/java/com/rodrigo/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,35 @@
+package com.rodrigo.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.rodrigo.core_ui.LocalSpacing
+import com.rodrigo.onboarding_presentation.R
+import com.rodrigo.onboarding_presentation.components.ActionButton
+
+@Composable
+fun WelcomeScreen() {
+    val spacing = LocalSpacing
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = com.rodrigo.core.R.string.welcome_text),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h1
+        )
+        Spacer(modifier = Modifier.height(spacing.current.spaceMedium))
+        ActionButton(text = stringResource(id = com.rodrigo.core.R.string.next),
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
