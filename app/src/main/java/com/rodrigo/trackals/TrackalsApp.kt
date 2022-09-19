96a0348e1c9308a0be871d5ee7d08d7157723019
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2022-09-19 15:19:25 -0300

    setting up preferences

---
+++ app/src/main/java/com/rodrigo/trackals/TrackalsApp.kt
@@ -0,0 +1,7 @@
+package com.rodrigo.trackals
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrackalsApp: Application()
---
+++ core/src/main/java/com/rodrigo/core/data/preferences/DefaultPreferences.kt
@@ -0,0 +1,101 @@
+package com.rodrigo.core.data.preferences
+
+import android.content.SharedPreferences
+import com.rodrigo.core.domain.preferences.Preferences
+import com.rodrigo.core.model.ActivityLevel
+import com.rodrigo.core.model.Gender
+import com.rodrigo.core.model.GoalType
+import com.rodrigo.core.model.UserInfo
+import java.util.function.Predicate
+
+class DefaultPreferences(
+    private val sharedPref: SharedPreferences
+) : Preferences {
+    override fun saveGender(gender: Gender) {
+        sharedPref
+            .edit()
+            .putString(Preferences.KEY_GENDER, gender.name)
+            .apply()
+    }
+
+    override fun saveAge(age: Int) {
+        sharedPref
+            .edit()
+            .putInt(Preferences.KEY_AGE, age)
+            .apply()
+    }
+
+    override fun saveWeight(weight: Float) {
+        sharedPref
+            .edit()
+            .putFloat(Preferences.KEY_WEIGHT, weight)
+            .apply()
+    }
+
+    override fun saveHeight(height: Int) {
+        sharedPref
+            .edit()
+            .putInt(Preferences.KEY_HEIGHT, height)
+            .apply()
+    }
+
+    override fun saveActivityLevel(level: ActivityLevel) {
+        sharedPref
+            .edit()
+            .putString(Preferences.KEY_ACTIVITY_LEVEL, level.name)
+            .apply()
+    }
+
+    override fun saveGoalType(type: GoalType) {
+        sharedPref
+            .edit()
+            .putString(Preferences.KEY_GOAL_TYPE, type.name)
+            .apply()
+    }
+
+    override fun saveCarbRatio(ratio: Float) {
+        sharedPref
+            .edit()
+            .putFloat(Preferences.KEY_CARB_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveProteinRatio(ratio: Float) {
+        sharedPref
+            .edit()
+            .putFloat(Preferences.KEY_PROTEIN_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveFatRatio(ratio: Float) {
+        sharedPref
+            .edit()
+            .putFloat(Preferences.KEY_FAT_RATIO, ratio)
+            .apply()
+    }
+
+    override fun loadUserInfo(): UserInfo {
+        val gender = sharedPref.getString(Preferences.KEY_GENDER, null)
+        val age = sharedPref.getInt(Preferences.KEY_AGE, -1)
+        val weight = sharedPref.getFloat(Preferences.KEY_WEIGHT, 1F)
+        val height = sharedPref.getInt(Preferences.KEY_HEIGHT, 1)
+        val activityLevel = sharedPref.getString(Preferences.KEY_ACTIVITY_LEVEL, null)
+        val goalType = sharedPref.getString(Preferences.KEY_GOAL_TYPE, null)
+        val carbRatio = sharedPref.getFloat(Preferences.KEY_CARB_RATIO, 2F)
+        val proteinRatio = sharedPref.getFloat(Preferences.KEY_PROTEIN_RATIO, 3F)
+        val fatRatio = sharedPref.getFloat(Preferences.KEY_FAT_RATIO, 4F)
+
+        return UserInfo(
+            gender = Gender.fromString(gender ?: "male"),
+            age = age,
+            weight = weight,
+            height = height,
+            activityLevel = ActivityLevel.fromString(activityLevel ?: "medium"),
+            goalType = GoalType.fromString(goalType ?: "keep_weight"),
+            carbRatio = carbRatio,
+            proteinRatio = proteinRatio,
+            fatRatio = fatRatio
+        )
+    }
+
+}
