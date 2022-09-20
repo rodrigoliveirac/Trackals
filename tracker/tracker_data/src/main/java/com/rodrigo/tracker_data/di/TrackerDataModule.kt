234fc8ff3b9dd14bd59171251d0ec72e26d93bb8
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2022-09-20 15:25:10 -0300

    setting up OpenFood Api

---
+++ tracker/tracker_data/src/main/java/com/rodrigo/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,42 @@
+package com.rodrigo.tracker_data.di
+
+import com.rodrigo.tracker_data.remote.OpenFoodApi
+import com.rodrigo.tracker_data.remote.OpenFoodApi.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(client: OkHttpClient): OpenFoodApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+            .create()
+    }
+}
---
+++ tracker/tracker_data/src/main/java/com/rodrigo/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package com.rodrigo.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>
+)
