66a8536f9e229c45742019dc4319b3977e2cd76a
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2022-09-29 16:43:51 -0300

    setting up Expandable Meal UI

--- tracker/tracker_presentation/src/main/java/com/rodrigo/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/rodrigo/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,7 +1,6 @@
 package com.rodrigo.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,6 +8,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.rodrigo.core.util.UiEvent
 import com.rodrigo.core_ui.LocalSpacing
+import com.rodrigo.tracker_presentation.components.DaySelector
+import com.rodrigo.tracker_presentation.tracker_overview.TrackerOverviewEvent.*
 import com.rodrigo.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -26,6 +27,16 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = { viewModel.onEvent(OnPreviousDayClick) },
+                onNextDayClick = { viewModel.onEvent(OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
---
+++ tracker/tracker_presentation/src/main/java/com/rodrigo/tracker_presentation/tracker_overview/components/ParseDateText.kt
@@ -0,0 +1,18 @@
+package com.rodrigo.tracker_presentation.tracker_overview.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.rodrigo.tracker_presentation.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun ParseDateText(date: LocalDate): String{
+    val today = LocalDate.now()
+    return when(date) {
+        today -> stringResource(id = com.rodrigo.core.R.string.today)
+        today.minusDays(1) -> stringResource(id = com.rodrigo.core.R.string.yesterday)
+        today.plusDays(1) -> stringResource(id = com.rodrigo.core.R.string.tomorrow)
+        else -> DateTimeFormatter.ofPattern("dd LLLL").format(date)
+    }
+}
